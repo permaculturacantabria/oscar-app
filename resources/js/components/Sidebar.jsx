e920887622c38206a1ac447033f50cf18ee5400b
oscar <permaculturacantabria@gmail.com>
2025-10-15 19:03:17 +0200

    instal node js

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import {
     LayoutDashboard,
-    NotebookPen,
     Users,
     FolderTree,
     Calendar,
